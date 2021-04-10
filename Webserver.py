5e73620f3afad2176a690faee097f76d2594b8e1
arpitgrewal12 <56005788+arpitgrewal12@users.noreply.github.com>
2021-04-10 01:59:05 -0700

    Update Webserver.py

--- Webserver.py
+++ Webserver.py
@@ -61,10 +61,7 @@ def start():
         except timeout:
             print("Timeout")
 
-        except (socket.timeout, socket.gaierror) as error:
-        logger.debug('telnet error: ', error)
-        delta = None
-        
+       
         
         conn.sendall(response.encode())
         conn.close()
