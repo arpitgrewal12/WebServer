2cbfd2baa1edaa479563bb8832226eca65967252
arpitgrewal12 <56005788+arpitgrewal12@users.noreply.github.com>
2021-04-13 13:38:01 -0700

    Update Webserver.py

--- Webserver.py
+++ Webserver.py
@@ -1,54 +1,122 @@
 import socket
 import threading
 import time
+import select
+import os
+import datetime
+import os.path
+#import stat
 HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
+x= None
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
-server.settimeout(10)
 
+def to_integer(dt_time):
+    return 10000*dt_time.year + 100*dt_time.month + dt_time.day
+    
+def never_stop():
+    time_started = time.time()
+    X = 2
+    while True:
+        if time.time() > time_started + X:
+            raise TimeoutException()
+        # response = 'HTTP/1.1 408 REQUEST TIME OUT'
+        
+        
+def modification_date(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
+
+def created_date(filename):
+    t = os.path.getctime(filename)
+    return datetime.datetime.fromtimestamp(t)
+
+
+def last_accessed(filename):
+    t= os.path.getatime(filename)
+    return datetime.datetime.fromtimestamp(t)
 
 def start():
+    x=  None
+    TIMEOUT=20.0
     server.listen()
     print(f"[LISTENING] Server is listening on {SERVER}")
+    
     while True:
+        
         conn, addr = server.accept()
+
+        ready = select.select([conn], [], [], TIMEOUT)
         
-        request = conn.recv(1024).decode()
-#        print(request)
-        
-        # Parse HTTP headers
-        headers = request.split('\n')
-        filename = headers[0].split()[1]
-
-        # Get the content of the file
-        if filename == '/':
-            filename = '/test.html'
-
-        try:
-            fin = open('/Users/arpitkaur/Desktop/Webserver'+filename)
-            content = fin.read()
-            fin.close()
-
-            response = 'HTTP/1.1 200 OK\n\n' + content
+        if ready[0] == []: #Timeout
+            response = 'HTTP/1.1 408 REQUEST TIME OUT'
             print(response)
+            print(response.encode())
+            conn.sendall(response.encode())
+            break
+
+        request = conn.recv(1024).decode()
+
+        headers = request.split('\n')
+        file = headers[0].split()[1]
+        method = headers[0].split()[0]
+        
+
+        if (file == '/'  or file == '/test.html'):
+            file = 'test.html'
+            
+
+        if (method != "GET"):
+            response = "HTTP/1.1 400 Bad Request\r\n\r\n"
+            conn.send(bytes(response, encoding='utf8'))
+            print(response)
+            break
+        
+        
+        try:
+            f = open(file)
+            content = f.read()
+            t1 = threading.Thread(target=never_stop, args=(2,))
+            f.close()
+   
+            
+#            print("Last modified: " ,modification_date(file))
+#            print("Date created:",created_date(file))
+#            print("Last access Time:", last_accessed(file))
+                       
+            last_modified= to_integer(modification_date(file))
+            date_created=created_date(file)
+            accessed=last_accessed(file)
+            
+            if request.__contains__('<body>') or request.__contains__('</body>'):
+                return 'HTTP/1.1 400 BAD REQUEST\n\n'
+            
+            if(x):
+#                print("x:",x)
+                if(last_modified < x):
+                    response = 'HTTP/1.1 304 NOT MODIFIED \n\n' + content
+                    print(response)
+                
+            else:
+                x=int(round(time.time()))
+#                print("x:",x)
+                response = 'HTTP/1.1 200 OK \n\n' + content
+                print(response)
             
         except FileNotFoundError:
-
             response = 'HTTP/1.1 404 NOT FOUND\n\nFile Not Found'
-            print (response)
-        except timeout:
-            print("Timeout")
+            print(response)
 
-       
-        
+    
         conn.sendall(response.encode())
-        conn.close()
+    conn.close()
+    server.close()
         
 
 
